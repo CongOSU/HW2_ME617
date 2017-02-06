54f8aab0121a918a9fe5f243c77d69bc50ecde5a
CongOSU <huangcon@oregonstate.edu>
2017-02-05 21:41:22 -0800

    Add files via upload
    Incomplete

--- HW2_4.py
+++ HW2_4.py
@@ -48,15 +48,63 @@ gears = [11, 23, 31, 47, 59, 71, 83, 97, 109, 127]
 
 
 def main():
-    l = []
+    l1 = []
     for i, gin in enumerate(gears):
-        for j, gout in enumerate(gears[i + 1:], i + 1):
+        # for j, gout in enumerate(gears):
+        for j, gout in enumerate(gears[i:], i):
             if check_distance(gin, gout):
                 name = str(i) + 'M' + str(j)
-                l.append(Node(name, Node(str(i), None)))
+                l1.append(Node(name, Node(str(i), None)))
 
-    for i in l:
-        print(i.name, i.parent.name, i.wc)
+    l2 = []
+    for i, g in enumerate(l1):
+        for j, gout in enumerate(gears):
+            gin = gears[int(g.name[-1])]
+            if check_distance(gin, gout) and j != int(g.parent.name):
+                name = g.name + 'M' + str(j)
+                l2.append(Node(name, g))
+            if gout != gin:
+                name = g.name + 'P' + str(j)
+                l2.append(Node(name, g))
+
+    l3 = []
+    for i, g in enumerate(l2):
+        for j, gout in enumerate(gears):
+            gin = gears[int(g.name[-1])]
+            if check_distance(gin, gout) and j != int(g.parent.name[-1]):
+                name = g.name + 'M' + str(j)
+                l3.append(Node(name, g))
+            if gout != gin and g.name[-2] != 'P':
+                name = g.name + 'P' + str(j)
+                l3.append(Node(name, g))
+
+    l4 = []
+    for i, g in enumerate(l3):
+        for j, gout in enumerate(gears):
+            gin = gears[int(g.name[-1])]
+            if check_distance(gin, gout) and j != int(g.parent.name[-1]):
+                name = g.name + 'M' + str(j)
+                l3.append(Node(name, g))
+            if gout != gin and g.name[-2] != 'P':
+                name = g.name + 'P' + str(j)
+                l3.append(Node(name, g))
+
+    for i in l3:
+        if calc_error(100, -117, i.wc) <= 2.5:
+            print("Found the smallest set of gears in group A:", i.name)
+        if calc_error(100, 77, i.wc) <= 2.5:
+            print("Found the smallest set of gears in group A:", i.name)
+        if calc_error(100, 377, i.wc) <= 2.5:
+            print("Found the smallest set of gears in group A:", i.name)
+        if calc_error(100, -20, i.wc) <= 2.5:
+            print("Found the smallest set of gears in group A:", i.name)
+        if calc_error(100, -2345, i.wc) <= 2.5:
+            print("Found the smallest set of gears in group A:", i.name)
+        if calc_error(100, 2, i.wc) <= 2.5:
+            print("Found the smallest set of gears in group A:", i.name)
+
+    # for i in ll:
+    #     print(i.name, i.parent.name, "{0:2.5}".format(i.wc))
 
 if __name__ == '__main__':
     main()
